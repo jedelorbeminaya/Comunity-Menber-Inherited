0cc71bfd87b44826e43e26e3009f7bbffbd4be32
Jose Eduardo Del Orbe Minaya <jedelorbeminaya@outlook.com>
2024-06-01 19:10:19 -0400

    starting-developmet

---
+++ ComunityMenberInherited/Maestro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComunityMenberInherited
+{
+    public class Maestro:Docente
+    {
+        public string? teacherType { get; set; }
+        public string? teacherArea { get; set; }
+    }
+}
